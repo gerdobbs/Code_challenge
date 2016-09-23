294311ac8cbf49d2af2c255ebe1883206b11eefe
gerdobbs <dobbsger0@gmail.com>
2016-09-23 22:13:59 +0100

    First

---
+++ LargestPrime.java
@@ -0,0 +1,34 @@
+/*
+ * 
+ * Name:		Gerard Dobbs
+ * College:		IT Carlow
+ * Date:		September 2016
+ * Purpose:     Check for the Highest Prime Factor of a Number. ( In this case 600851475143)
+ * Method:		Divide 600851475143 (largeNumber) by 2(factorCheck) initially to find largest factor
+				If it does not divide evenly keep increase factorCheck until highest factor is found
+				LargeNumber is then set to factorCheck and process repeated until the factor which is a prime is found
+ */
+public class LargestPrime {
+
+	private static long largeNumber = 600851475143L;  
+	private static long factorCheck = 2L;
+	
+	public static void main(String[] args)
+	{
+		System.out.print("The largest prime factor of " + largeNumber +" is " +checkFactor());
+	}
+	
+	private static  long checkFactor()
+	{
+		while(Math.sqrt(largeNumber) > factorCheck)		//Stop search after square root is reached	
+		{
+			if(largeNumber % factorCheck == 0){			
+				largeNumber = largeNumber/factorCheck;	//Divides evenly to give next highest factor. Is it a prime?
+			}
+			else{
+				factorCheck++;							//Does not divide evenly so check for factor again
+			}
+		}
+		return largeNumber;
+	}
+}
---
+++ Palindrome.java
@@ -0,0 +1,47 @@
+/*
+ * Name:			Gerard Dobbs
+ * College:			IT Carlow
+ * Date:			September 2016
+ * Purpose:			To find the largest palindrome made from the product of two 3-digit numbers.
+ * Method:			Start out with a variable set to 999 and multiply it by itself.
+ * 					Reduce 1st number by 1, multiply it by numbers => than it.
+ * 					Continue Reducing the variable.
+ * 					After each multiplication check to see if it is a palindrome.
+ * 					To do this isolate the last digit by getting remainder of mod10.
+ * 					Remainder will be 1st number of next digit
+ * 					Continue getting remainder & adding to end of new digit, which is formed from the quotients,
+ * 					If its a palindrome compare to current largest palindrome, 					
+ */
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		int number= 999;
+		int largestPal=0;
+		
+		for(int i= number; i>=0; i--)				 
+		{
+			for(int j=number; j>=i; j--)			//Multiply first number by number >= firdt Number
+			{
+				if((i*j) == reverseResult(i*j))		//Is it a Palindrome
+				{
+					if(i*j>largestPal)			
+						largestPal=i*j;				//New largest palindrome
+				}
+			}
+		}
+		System.out.println("The largest palindrome made from the product of two 3-digit numbers is " +(largestPal));
+	}
+	public static int reverseResult(int numToReverse){
+		int reversedNum=0;
+		int tempNum = 0;							 //Variable to stpre last digit each time
+		while(numToReverse>0){
+			tempNum = numToReverse%10;				 //Isolate last digit
+			reversedNum = reversedNum*10 + tempNum;  //Move decimal point and add 
+			numToReverse = numToReverse/10;			 //Continue with New number without last digit
+		}
+		return reversedNum;
+	}
+	
+
+}
